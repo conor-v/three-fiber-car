ba2a450394a984e9220c4f9e1f43632de835863f
conor-v <conor.vanoystaeyen@hotmail.com>
2022-08-11 17:43:46 +0200

    animaties

--- src/Ground.js
+++ src/Ground.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { MeshReflectorMaterial } from "@react-three/drei";
 import { LinearEncoding, RepeatWrapping, TextureLoader } from "three";
 
@@ -20,6 +20,12 @@ export const Ground = () => {
 
     normal.encoding = LinearEncoding;
   }, [normal, roughness]);
+
+  useFrame((state, delta) => {
+    let t = -state.clock.getElapsedTime() * 0.128
+    roughness.offset.set(0, t)
+    normal.offset.set(0, t)
+   })
 
   return (
     <mesh rotation-x={-Math.PI * 0.5} castShadow receiveShadow>
